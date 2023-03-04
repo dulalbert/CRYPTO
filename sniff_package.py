4ba2b1d0745038407f76338cfc61de28f1268e99
albertdu <albert.dulout@imt-atlantique.net>
2023-03-04 15:23:08 +0100

    ajout script scappy

--- sniff_package.py
+++ sniff_package.py
@@ -1,17 +1,42 @@
 import netifaces
 from scapy.all import *
 import pandas as pd
+import datetime
+
+window = 4 
 
 # On garde que les ports wifi et ethernet
 interfaces = list(filter(lambda s: ('en' or 'eth') in s, netifaces.interfaces()))
 
-# Capturer 100 packets
-pkt = sniff(iface=interfaces, count=100)
+# Capturer 200 packets
+pkt = sniff(iface=interfaces, count=200)
 
 data = []
 for packet in pkt:
-    data.append([packet.sniffed_on ,packet.time, packet.src, packet.dst, len(packet)])
+    if IP in packet : # garder seulement packet IP
+        data.append([packet.sniffed_on ,packet.time, packet[IP].src, packet[IP].dst, len(packet)])
 
 sniffed_df = pd.DataFrame(data, columns=['interface','Time', 'Source', 'Destination', 'Length'])
 
-sniffed_df.to_csv('sniffed_df.csv', index = False)
+#Filtrer sur l'interface la plus utilisée
+most_used_interface = sniffed_df.groupby(by = 'interface').sum('Length').nlargest(1, 'Length').iloc[0].name
+sniffed_df = sniffed_df.where(sniffed_df["interface"] == most_used_interface).drop("interface", axis = 1)
+
+# Add a column outbound
+ip = get_if_addr(most_used_interface)
+sniffed_df['outbound'] = sniffed_df['Source'].apply(lambda x : x == ip)
+
+sniffed_df['delta'] = sniffed_df.Time.diff()
+
+# Delta Rolling average + Standard Deviation
+sniffed_df['ra_delta'] = sniffed_df.delta.rolling(window=window).mean()
+sniffed_df['rstd_delta'] = sniffed_df.delta.rolling(window=window).std()
+
+# Delta Rolling average + Standard Deviation
+sniffed_df['ra_lenght'] = sniffed_df.Length.rolling(window=window).mean()
+sniffed_df['rstd_lenght'] = sniffed_df.Length.rolling(window=window).std()
+# remove useless columns
+sniffed_df.drop(['Time', 'Source', 'Destination'], axis = 1, inplace = True)
+sniffed_df.dropna(inplace = True)
+
+sniffed_df.to_csv(f'cryptojacking/network_sniff/scappy-{datetime.datetime.now()}', index = False)
