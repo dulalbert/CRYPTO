ec57d86b4b434c1839819f0c42cd71e380a07340
albertdu <albert.dulout@imt-atlantique.net>
2023-03-13 23:31:04 +0100

    exported model

--- train_xgboost.py
+++ train_xgboost.py
@@ -1,22 +1,24 @@
-    """Entrainer le modèle de ML permettant la détection
-    """
-
+"""
+Author : Albert Dulout
+Entrainer le modèle de ML permettant la détection
+"""
 from os import getcwd, path
 from glob import glob
+import pkg_resources
+
 import pandas as pd
 from xgboost import XGBClassifier
-
+pkg_resources.require('xgboost == 1.7.3')
 WINDOW = 4
 
 def prepare_sniff_wireshark(sniffed_df : pd.DataFrame):
- """Applique le Feature Engineering choisi
-    Args:
+    """ Applique le Feature Engineering choisi
+     Args:
         sniffed_df (pd.DataFrame): Dataframe provenant de scapy
 
     Returns:
         cleaned_sniffed_df: Feature Engineering appliqué
     """
-
     sniffed_df['delta'] = sniffed_df.Time.diff()
     # Delta Rolling average + Standard Deviation
     sniffed_df['ra_delta'] = sniffed_df.delta.rolling(window=WINDOW).mean()
@@ -32,8 +34,8 @@ def prepare_sniff_wireshark(sniffed_df : pd.DataFrame):
 
 mining_DF = pd.DataFrame(columns=['No.','Length', 'delta', 'ra_delta', 'rstd_delta', 'ra_lenght',
        'rstd_lenght', 'miner'])
-non_mining_DF = pd.DataFrame(columns=['No.','Length', 'delta', 'ra_delta', 'rstd_delta', 'ra_lenght',
-       'rstd_lenght', 'miner'])
+non_mining_DF = pd.DataFrame(columns=['No.','Length', 'delta', 'ra_delta',
+                                       'rstd_delta', 'ra_lenght', 'rstd_lenght', 'miner'])
 for name in glob(path.join(getcwd()+"/network_sniff/", "*minage*.csv")):
     _ = pd.read_csv(name)
     _.pipe(prepare_sniff_wireshark)
@@ -48,3 +50,12 @@ for name in glob(path.join(getcwd()+"/network_sniff/", "*calme*.csv")):
 
 training_DF = pd.concat([mining_DF,non_mining_DF], ignore_index= True)
 training_DF[['No.', 'Length']] = training_DF[['No.', 'Length']].astype('int')
+
+y = training_DF['miner']
+X = training_DF.drop('miner', axis = 1)
+
+fix_params = {'learning_rate': 0.2, 'n_estimators': 100, 'objective': 'binary:logistic', 'max_depth': 5, 'min_child_weight': 2}   #other parameters, fixed for the moment
+xg = XGBClassifier(**fix_params)
+
+xg.fit(X, y)
+xg.save_model('model.bst')
